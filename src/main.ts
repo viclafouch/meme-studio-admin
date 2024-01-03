344d48316e3365d0fd745fbfe8f906c7f519b7e6
Victor de la Fouchardiere <victor.delafouchardiere@frichti.co>
2024-01-03 13:53:20 +0100

    chore: PORT from process env

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
 
